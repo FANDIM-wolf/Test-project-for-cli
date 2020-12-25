6840412c671e5a29f3cf6566f059be0da9a6ff5a
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2020-12-25 12:51:58 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Mycomponent/>
+   
+  </div>
+</template>
+
+<script>
+import Mycomponent from './components/Mycomponent'
+export default {
+  name: 'App',
+  
+  components: {
+    Mycomponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Mycomponent.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="root">
+	<label>
+	Enter email:
+	<input type="email" v-bind:value="email" @input="emailInput($event.target.value)" name=""> 	
+	</label>
+	<label>
+	Enter password:
+	<input type="password" v-bind:value="password" @input="passwordInput($event.target.value)" name=""> 	
+	</label>
+	<br>
+	<button @click="count--">-</button>
+	<input type="number" v-bind:value="count"  name="">
+    <button @click="count++">+</button>
+
+
+		
+	</div>
+</template>
+
+<script >
+	export default{
+		name: 'Mycomponent',
+		data(){
+               
+        return{text:"Lafore",
+               email:'example@mail.ru',
+               password:'12345678',
+               count:0
+    }
+	},
+	methods:{
+		emailInput(value){
+           this.email= value //new value of email
+		},
+		passwordInput(value){
+			this.password = value
+		}
+	}
+
+}
+</script>
