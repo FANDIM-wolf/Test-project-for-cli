964e3470ac28ddcb054777ce03caf584ab6f044e
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2020-12-28 22:36:23 +0300

    Add files via upload

--- src/components/Mycomponent.vue
+++ src/components/Mycomponent.vue
@@ -6,7 +6,11 @@
         
         <div class="info">
           <h2>{{ getAuthorFullName }}</h2>
+
           <strong>Бизнес-коуч</strong>
+          <li v-for="item in items" :key="item.message">
+             {{ item.message }}
+          </li>
           <ul class="list">
             <li>Учу делать РЕАЛЬНЫЕ бабки, на себя просто не хватило времени</li>
             <li>НЕ знаю, что такое "Успешный Успех", но знаю, что нужно ТЕБЕ!</li>
@@ -14,38 +18,10 @@
           </ul>
         </div>
       </div>
-      <p>Участников: {{ users.length }}</p>
-      <ul class="list">
-        <li
-          v-for="(user, index) in users"
-          :key="index"
-        >
-          {{ `${index + 1}. ${getFullName(user)}` }}
-        </li>
-      </ul>
-      <button
-        type="button"
-        @click="currentPage--"
-      >
-        Пред.
-      </button>
-      <button
-        type="button"
-        v-for="page in pages"
-        :key="page"
-        @click="currentPage = page"
-      >
-        {{ page }}
-      </button>
-      <button
-        type="button"
-        @click="currentPage++"
-      >
-        След.
-      </button>
-      <p>Страница: {{ currentPage }} из {{ pages }}</p>
     </div>
   </div>
+  
+ 
 </template>
 
 <script>
@@ -56,25 +32,10 @@ export default {
       firstName: 'Игнатий',
       secondName: 'Иларионович',
       lastName: 'Богатов',
-      users: [
-        {
-          firstName: 'Игнатий',
-          secondName: 'Иларионович',
-          lastName: 'Богатов'
-        },
-        {
-          firstName: 'Игнатий',
-          secondName: 'Иларионович',
-          lastName: 'Богатов'
-        },
-        {
-          firstName: 'Игнатий',
-          secondName: 'Иларионович',
-          lastName: 'Богатов'
-        }
-      ],
-      pages: 3,
-      currentPage: 1
+      items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
     }
   },
   computed: {
@@ -83,29 +44,13 @@ export default {
     }
   },
   methods: {
-    getFullName(user) {
-      return `${user.firstName} ${user.secondName} ${user.lastName}`
-    },
-    loadUsers(page) {
-      console.log(`Загрузка пользователей, страница - ${page}`)
-    }
+   
+  },
     
 
-  },
+  
   watch: {
-    currentPage(page) {
-      this.loadUsers(page)
-    },
-    increaseCount(page) {
-      if (this.page < 1) {
-        this.page++
-      }
-    },
-    decreaseCount(page) {
-      if (this.page > 3) {
-        this.page--
-     }
-    }
+    
     
   }
 }
